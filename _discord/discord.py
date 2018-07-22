6e78eb875497c65806f653c7fdcc2fc1019b6a29
Carl Colglazier <carl@carlcolglazier.com>
2018-07-22 17:12:55 -0400

    Use Atom feed for subreddit(s) feed.

--- _discord/discord.py
+++ _discord/discord.py
@@ -15,9 +15,11 @@ rs = reddit.RedditSession(
 )
 bot = commands.Bot(command_prefix='%')
 # TODO: Put this in a database.
+"""
 badwords = []
+"""
 
-@bot.command(name='ping')
+@bot.command(name='ping', pass_context=True)
 async def ping(ctx):
     """
     Ping command for testing that the bot is online.
@@ -25,7 +27,10 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command(name='notes')
-async def ntoes(ctx, user):
+async def notes(ctx, user):
+    """
+    Get the usernotes from moderator toolbox for a user.
+    """
     usernotes = rs.get_usernotes(SUBREDDIT)
     if user not in usernotes:
         await ctx.send("No notes found for {}.".format(user))
@@ -33,6 +38,8 @@ async def ntoes(ctx, user):
         print(usernotes)
         await ctx.send("{}".format(usernotes[user]))
 
+# TODO: Connect this to a database.
+"""
 @bot.command(name='badword')
 async def badword(ctx, command, word):
     if command == 'list':
@@ -49,32 +56,34 @@ async def badword(ctx, command, word):
             await ctx.send("{} removed from the list of bad words.".format(word))
         else:
             await ctx.send("{} wasn't found in the list of bad words.".format(word))
+"""
 
 async def reddit_feed():
     await bot.wait_until_ready()
     start_time = time.time()
-    submissions = rs.stream(SUBREDDITS).submissions(pause_after=0)
+    #submissions = rs.stream(SUBREDDITS).submissions(pause_after=0)
+    feed = reddit.SubmissionFeed(SUBREDDITS, 60)
     feed_chan = bot.get_channel(FEED_CHANNEL)
     while not bot.is_closed():
-        submission = next(submissions)
-        if submission is None:
-            print("Waiting...")
-            await asyncio.sleep(15)
-        elif submission.created_utc > start_time:
+        for sub in feed.submission():
             embed = discord.Embed(
-                title=submission.title,
-                description="New post in /r/{}".format(submission.subreddit),
-                url=submission.shortlink,
+                title=sub.title,
+                description="New post in {}".format(sub.subreddit),
+                url=sub.url,
                 color=0x00ff00
             )
             # TODO: This is a shortcut.
-            if 'png' in submission.url or 'jpg' in submission.url:
-                embed = embed.set_image(url=submission.url)
+            # Refine this for better support of other image formats.
+            # Consider using thumbnails as well if the post is a link.
+            # Basically just make this richer.
+            if 'png' in sub.link or 'jpg' in sub.link:
+                embed = embed.set_image(url=sub.link)
             embed = embed.set_author(
-                name=submission.author,
-                url="https://reddit.com/u/{}".format(submission.author)
+                name=sub.author,
+                url="https://reddit.com{}".format(sub.author)
             )
             await feed_chan.send(embed=embed)
+        await asyncio.sleep(60)
 
 @bot.event
 async def on_ready():
--- reddit/reddit.py
+++ reddit/reddit.py
@@ -2,6 +2,13 @@ import praw
 import json
 import base64
 import zlib
+import feedparser
+import datetime
+from bs4 import BeautifulSoup 
+
+REDDIT_RSS = 'https://www.reddit.com/r/{}/new/.rss?sort=new&limit={}'
+FEED_LIMIT = 5
+PERIOD = 60
 
 class RedditSession:
     def __init__(self,
@@ -24,3 +31,56 @@ class RedditSession:
 
     def stream(self, communities):
         return self.reddit.subreddit('+'.join(communities)).stream
+
+class Submission:
+    """
+    def __init__(self, title, subreddit, url, link, author):
+        self.title = title
+        self.subreddit = subreddit
+        self.url = url
+        self.link = link
+        self.author = author
+    """
+
+    def __init__(self, dictionary):
+        soup = BeautifulSoup(dictionary['description'], 'html.parser')
+        mdclass = soup.find('div',class_='md')
+        if mdclass is not None:
+            mdclass.decompose()
+        links = soup.find_all('a')
+        if links[0].find('img'):
+            # This could possibly be used to get the thumbnail.
+            del links[0]
+        self.title = dictionary['title']
+        self.subreddit = links[1].get_text().strip()
+        self.url = links[3]['href']
+        self.link = links[2]['href']
+        self.author = links[0].get_text().strip()
+    
+class SubmissionFeed:
+    def __init__(self, communities, interval):
+        self.communities = communities
+        self.interal = interval
+        self.last = (
+            datetime.datetime.utcnow() - datetime.timedelta(minutes=PERIOD)
+        ).timetuple()
+        self.submissions = []
+
+    def atom_feed(self):
+        subs_list = '+'.join(self.communities)
+        feed = feedparser.parse(
+            REDDIT_RSS.format(subs_list, FEED_LIMIT)
+        )
+        return feed
+
+    def new_submissions(self):
+        feed = self.atom_feed()
+        new = [Submission(x) for x in feed['entries'] if x['updated_parsed'] > self.last]
+        self.last = datetime.datetime.utcnow().timetuple()
+        return new
+
+    def submission(self):
+        if len(self.submissions) == 0:
+            self.submissions = self.new_submissions()
+        while len(self.submissions):
+            yield self.submissions.pop(0)
